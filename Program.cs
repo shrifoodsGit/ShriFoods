1e0041fd82e9a54f5e4e2f8afb83f113ae2445b7
shrifoodsGit <shrifoodspb@gmail.com>
2026-05-10 21:26:52 +0530

    Updating SMTP

--- Program.cs
+++ Program.cs
@@ -30,8 +30,6 @@ builder.Services.Configure<EmailSettings>(
 
 builder.Services.AddScoped<EmailService>();
 
-Environment.GetEnvironmentVariable("Smtp_Brevo_Key");
-Environment.GetEnvironmentVariable("MSG91_key");
 
 //SmsService
 builder.Services.AddScoped<SmsService>();
